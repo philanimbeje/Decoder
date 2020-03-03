7594caa1fb81d8765d9e16e6cb006f73433cd299
Philani Mbeje <philani.mbeje@conlog.com>
2020-03-03 14:09:21 +0200

    Add project files.

---
+++ DecoderLibrary/Encoder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Runtime.Security;
+
+namespace DecoderLibrary
+{
+    public static class Encoder
+    {
+        public static string Encrypt(string connectionString)
+        {
+            try
+            {
+                var result = SimpleStringCipher.Instance.Encrypt(connectionString);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        public static string Decrypt(string connectionString)
+        {
+            try
+            {
+                var result = SimpleStringCipher.Instance.Decrypt(connectionString);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
---
+++ DecoderWPF/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DecoderWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncode_Click(object sender, RoutedEventArgs e)
+        {
+            txtResult.Text = DecoderLibrary.Encoder.Encrypt(txtConnectionString.Text);
+        }
+
+        private void btnDecode_Click(object sender, RoutedEventArgs e)
+        {
+            txtResult.Text = DecoderLibrary.Encoder.Decrypt(txtEncryptedString.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            txtConnectionString.Text = "";
+        }
+
+        private void btnClearEncrypted_Click(object sender, RoutedEventArgs e)
+        {
+            txtEncryptedString.Text = "";
+        }
+
+        private void txtClearResult_Click(object sender, RoutedEventArgs e)
+        {
+            txtResult.Text = "";
+        }
+    }
+}
